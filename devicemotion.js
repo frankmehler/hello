30108d1cc6ddb8bbcc419aba0414c0d285aa3aff
frankmehler <36157035+frankmehler@users.noreply.github.com>
2018-02-12 19:59:10 +0100

    Add files via upload

--- devicemotion.js
+++ devicemotion.js
@@ -21,18 +21,18 @@ if (window.DeviceMotionEvent) {
 function accelerationHandler(acceleration, targetId) {
     var info, xyz = "[X, Y, Z]";
 
-    info = xyz.replace("X", acceleration.x && acceleration.x.toFixed(3));
-    info = info.replace("Y", acceleration.y && acceleration.y.toFixed(3));
-    info = info.replace("Z", acceleration.z && acceleration.z.toFixed(3));
+    info = xyz.replace("X", acceleration.x && acceleration.x.toFixed(1));
+    info = info.replace("Y", acceleration.y && acceleration.y.toFixed(1));
+    info = info.replace("Z", acceleration.z && acceleration.z.toFixed(1));
     document.getElementById(targetId).innerHTML = info;
 }
 
 function rotationHandler(rotation) {
     var info, xyz = "[X, Y, Z]";
 
-    info = xyz.replace("X", rotation.alpha && rotation.alpha.toFixed(3));
-    info = info.replace("Y", rotation.beta && rotation.beta.toFixed(3));
-    info = info.replace("Z", rotation.gamma && rotation.gamma.toFixed(3));
+    info = xyz.replace("X", rotation.alpha && rotation.alpha.toFixed(2));
+    info = info.replace("Y", rotation.beta && rotation.beta.toFixed(2));
+    info = info.replace("Z", rotation.gamma && rotation.gamma.toFixed(2));
     document.getElementById("moRotation").innerHTML = info;
 }
 
