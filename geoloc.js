0ff67fa39468d3e09fc7f002f65f5bd76ed3cdb0
frankmehler <36157035+frankmehler@users.noreply.github.com>
2018-02-13 18:56:35 +0100

    Add files via upload

--- geoloc.js
+++ geoloc.js
@@ -1,36 +1,38 @@
-if (navigator.geolocation) {
-    var options = {
-        enableHighAccuracy: true,
+function holePosition() {
+    if (navigator.geolocation) {
+        document.getElementById("geoSupported").innerText = "OK - Geolocation wird unterstützt!";
+        var options = {
+            enableHighAccuracy: true
+        }
+        navigator.geolocation.getCurrentPosition(showPosition, showError, options);
+    } else {
+        document.getElementById("geoSupported").innerText = "Geolocation wird nicht unterstützt!";
     }
-    navigator.geolocation.getCurrentPosition(showPosition, showError, options);
-} else {
-    alert('Ihr Browser unterstützt die W3C Geolocation API nicht.');
 }
 
 function showPosition(position) {
-    alert(
-        'Die Geoposition dieses Geräts ist (Stand: ' + new Date(position.timestamp).toLocaleTimeString() + '):\n' +
-        'Breitengrad: ' + position.coords.latitude + '° \n' +
-        'Längengrad: ' + position.coords.longitude + '° \n' +
-        '  Genauigkeit: ' + position.coords.accuracy + 'm\n' +
-        (position.coords.altitude ? ('Höhe: ' + position.coords.altitude + 'm\n' +
-            '  Genauigkeit: ' + position.coords.altitudeAccuracy + 'm') : "")
-    );
+    document.getElementById("breite").innerHTML = 'Breitengrad: ' + position.coords.latitude;
+    document.getElementById("laenge").innerHTML = 'Längengrad: ' + position.coords.longitude;
+    document.getElementById("genau").innerHTML = 'Genauigkeit: ' + position.coords.accuracy + 'm';
+    document.getElementById("hoch").innerHTML = 'Höhe: ' + position.coords.altitude;
+    document.getElementById("genauhoch").innerHTML = 'Genauigkeit Höhe: ' + position.coords.altitudeAccuracy + 'm';
+    //'Höhe: ' + position.coords.altitude 
+    //'Genauigkeit Höhe: ' + position.coords.altitudeAccuracy
 }
 
 function showError(error) {
     switch (error.code) {
         case error.PERMISSION_DENIED:
-            alert('Sie haben die Abfrage ihrer Geoposition untersagt.');
+            alert('Abfrage der Geoposition untersagt.');
             break;
         case error.POSITION_UNAVAILABLE:
             alert('Es sind keine Geopositionsdaten verfügbar.');
             break;
         case error.TIMEOUT:
-            alert('Das Timeout für die Ortsanfrage wurde überschritten.');
+            alert('Timeout überschritten.');
             break;
         default:
-            alert('Es ist ein unbekannter Fehler aufgetreten (#' + error.code + ': ' + error.message + ')');
+            alert('Unbekannter Fehler');
             break;
     }
 }
