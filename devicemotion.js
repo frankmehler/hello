5c79e152f515017d19ff5c038945a73505196035
frankmehler <36157035+frankmehler@users.noreply.github.com>
2018-02-13 09:50:13 +0100

    Add files via upload

--- devicemotion.js
+++ devicemotion.js
@@ -1,23 +1,15 @@
-maxiX = -1;
-maxiY = -1;
-maxiZ = -1;
+maxi = -1;
+
 if (window.DeviceMotionEvent) {
     document.getElementById("dmeSupported").innerText = "OK - Device Motion wird unterstützt!";
     window.addEventListener('devicemotion', function(event) {
         document.getElementById("xBeschl").innerHTML = 'x ' + event.acceleration.x.toFixed(2);
         document.getElementById("yBeschl").innerHTML = 'y ' + event.acceleration.y.toFixed(2);
         document.getElementById("zBeschl").innerHTML = 'z ' + event.acceleration.z.toFixed(2);
-        if (event.acceleration.x > maxiX) {
-            maxiX = event.acceleration.x;
-            document.getElementById("meinMaxX").innerHTML = 'max x: ' + maxiX;
-        }
-        if (event.acceleration.y > maxiY) {
-            maxiY = event.acceleration.y;
-            document.getElementById("meinMaxY").innerHTML = 'max y: ' + maxiY;
-        }
-        if (event.acceleration.z > maxiZ) {
-            maxiZ = event.acceleration.z;
-            document.getElementById("meinMaxZ").innerHTML = 'max z: ' + maxiZ;
+
+        if (event.acceleration.z > maxi) {
+            maxi = event.acceleration.z;
+            document.getElementById("meinMax").innerHTML = 'max z: ' + maxi;
         }
     });
 } else {
