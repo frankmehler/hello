8fa1fb8712c9646dcef081b8865c68facac501c2
frankmehler <36157035+frankmehler@users.noreply.github.com>
2018-02-13 09:37:31 +0100

    Add files via upload

--- devicemotion.js
+++ devicemotion.js
@@ -8,11 +8,11 @@ if (window.DeviceMotionEvent) {
         document.getElementById("yBeschl").innerHTML = 'y ' + event.acceleration.y.toFixed(2);
         document.getElementById("zBeschl").innerHTML = 'z ' + event.acceleration.z.toFixed(2);
         if (event.acceleration.x > maxiX) {
-            maxiX = event.acceleration.z;
+            maxiX = event.acceleration.x;
             document.getElementById("meinMaxX").innerHTML = 'max x: ' + maxiX;
         }
         if (event.acceleration.y > maxiY) {
-            maxiY = event.acceleration.z;
+            maxiY = event.acceleration.y;
             document.getElementById("meinMaxY").innerHTML = 'max y: ' + maxiY;
         }
         if (event.acceleration.z > maxiZ) {
