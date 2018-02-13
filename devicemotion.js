adaf168ec128287eaf386f1a1edabb01bf67b25f
frankmehler <36157035+frankmehler@users.noreply.github.com>
2018-02-13 09:32:41 +0100

    Add files via upload

--- devicemotion.js
+++ devicemotion.js
@@ -1,33 +1,25 @@
+maxiX = -1;
+maxiY = -1;
+maxiZ = -1;
 if (window.DeviceMotionEvent) {
     document.getElementById("dmeSupported").innerText = "OK - Device Motion wird unterstützt!";
     window.addEventListener('devicemotion', function(event) {
         document.getElementById("xBeschl").innerHTML = 'x ' + event.acceleration.x.toFixed(2);
         document.getElementById("yBeschl").innerHTML = 'y ' + event.acceleration.y.toFixed(2);
         document.getElementById("zBeschl").innerHTML = 'z ' + event.acceleration.z.toFixed(2);
+        if (event.acceleration.x > maxiX) {
+            maxi = event.acceleration.z;
+            document.getElementById("meinMaxX").innerHTML = 'max x: ' + maxiX;
+        }
+        if (event.acceleration.y > maxiY) {
+            maxi = event.acceleration.z;
+            document.getElementById("meinMaxY").innerHTML = 'max y: ' + maxiY;
+        }
+        if (event.acceleration.z > maxiZ) {
+            maxi = event.acceleration.z;
+            document.getElementById("meinMaxZ").innerHTML = 'max z: ' + maxiZ;
+        }
     });
 } else {
     document.getElementById("dmeSupported").innerText = "Device Motion wird nicht unterstützt!";
-}
-
-maxi = -1;
-
-function zeigeBeschleunigung() {
-    document.getElementById("y").style.color = "red";
-    document.getElementById("meinMax").innerHTML = 'max: ' + maxi;
-    //Event mit Parameter: https://www.w3schools.com/js/js_htmldom_eventlistener.asp
-    window.addEventListener('devicemotion', function(event) {
-        eventBearbeiten(event);
-    });
-}
-
-function eventBearbeiten(event) {
-    // siehe https://wiki.selfhtml.org/wiki/JavaScript/Objekte/Number/toFixed
-    document.getElementById("z").style.color = "green";
-    document.getElementById("x").innerHTML = 'x ' + event.acceleration.x.toFixed(2);
-    document.getElementById("y").innerHTML = 'y ' + event.acceleration.y.toFixed(2);
-    document.getElementById("z").innerHTML = 'z ' + event.acceleration.z.toFixed(2);
-    if (event.acceleration.z > maxi) {
-        maxi = event.acceleration.z;
-        document.getElementById("meinMax").innerHTML = 'max: ' + maxi;
-    }
 }
