2d5f73ed355e4e6c80387d5a16a887e80155f7b2
frankmehler <36157035+frankmehler@users.noreply.github.com>
2018-06-27 09:58:31 +0200

    Add files via upload

--- js/geoloc.js
+++ js/geoloc.js
@@ -14,22 +14,7 @@ function holePosition() {
 function showPosition(position) {
     document.getElementById("breite").innerHTML = 'Breitengrad: ' + position.coords.latitude;
     document.getElementById("laenge").innerHTML = 'Längengrad: ' + position.coords.longitude;
-    var lat = position.coords.latitude;
-    var lon = position.coords.longitude;
-    var latlon = new google.maps.LatLng(lat, lon)
-    var mapholder = document.getElementById('mapholder')
-    mapholder.style.height = '250px';
-    mapholder.style.width = '500px';
-
-    var myOptions = {
-        center: latlon,
-        zoom: 14,
-    }
-    var map = new google.maps.Map(document.getElementById("mapholder"), myOptions);
-    var marker = new google.maps.Marker({ position: latlon, map: map, title: "You are here!" });
 }
-
-
 // Fortsetzung des Scripts geoloc.js:
 function showError(error) {
     switch (error.code) {
