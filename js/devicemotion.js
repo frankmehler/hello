e406800b22436e78dff260950ef921745ec74237
frankmehler <36157035+frankmehler@users.noreply.github.com>
2018-08-02 12:05:39 +0200

    Add files via upload

--- js/devicemotion.js
+++ js/devicemotion.js
@@ -1,16 +1,15 @@
-// Inhalt des Scripts devicemotion.js:
-maxi = -1;
 if (window.DeviceMotionEvent) {
-    document.getElementById("dmeSupported").innerText = "OK - Device Motion wird unterstützt!";
+    document.getElementById("dmeSupported").innerText = "Device Motion wird unterstützt!";
     window.addEventListener('devicemotion', function(event) {
-        document.getElementById("xBeschl").innerHTML = 'x ' + event.acceleration.x.toFixed(2);
-        document.getElementById("yBeschl").innerHTML = 'y ' + event.acceleration.y.toFixed(2);
-        document.getElementById("zBeschl").innerHTML = 'z ' + event.acceleration.z.toFixed(2);
-        if (event.acceleration.z > maxi) {
-            maxi = event.acceleration.z;
-            document.getElementById("meinMax").innerHTML = 'max z: ' + maxi;
-        }
+        document.getElementById("xBeschl").innerHTML = 'Beschleunigung in x-Richtung ' + event.acceleration.x.toFixed(1);
+        document.getElementById("yBeschl").innerHTML = 'Beschleunigung in y-Richtung ' + event.acceleration.y.toFixed(1);
+        document.getElementById("zBeschl").innerHTML = 'Beschleunigung in z-Richtung ' + event.acceleration.z.toFixed(1);
+
+        document.getElementById("xRotation").innerHTML = 'Rotationsrate beta um x-Achse: ' + event.rotationRate.alpha.toFixed(2);
+        document.getElementById("yRotation").innerHTML = 'Rotationsrate gamma um y-Achse: ' + event.rotationRate.beta.toFixed(2);
+        document.getElementById("zRotation").innerHTML = 'Rotationsrate alpha um z-Achse: ' + event.rotationRate.gamma.toFixed(2);
     });
+
 } else {
-    document.getElementById("dmeSupported").innerText = "Device Motion wird nicht unterstützt!";
+    document.getElementById("dmeSupported").innerText = "Device Motion wird nicht unterstützt!";
 }
