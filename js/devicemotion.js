6062a9cc7a303d16c2dcada5a0ef0fe3ebeed009
frankmehler <36157035+frankmehler@users.noreply.github.com>
2019-08-31 13:47:00 +0200

    Add files via upload

--- js/devicemotion.js
+++ js/devicemotion.js
@@ -1,15 +1,12 @@
+"use strict";
+
 if (window.DeviceMotionEvent) {
-    document.getElementById("dmeSupported").innerText = "Device Motion wird unterstützt!";
-    window.addEventListener('devicemotion', function(event) {
-        document.getElementById("xBeschl").innerHTML = 'Beschleunigung in x-Richtung ' + event.acceleration.x.toFixed(1);
-        document.getElementById("yBeschl").innerHTML = 'Beschleunigung in y-Richtung ' + event.acceleration.y.toFixed(1);
-        document.getElementById("zBeschl").innerHTML = 'Beschleunigung in z-Richtung ' + event.acceleration.z.toFixed(1);
-
-        document.getElementById("xRotation").innerHTML = 'Rotationsrate beta um x-Achse: ' + event.rotationRate.alpha.toFixed(2);
-        document.getElementById("yRotation").innerHTML = 'Rotationsrate gamma um y-Achse: ' + event.rotationRate.beta.toFixed(2);
-        document.getElementById("zRotation").innerHTML = 'Rotationsrate alpha um z-Achse: ' + event.rotationRate.gamma.toFixed(2);
+    document.getElementById("dmeSupported").innerText = "OK - Device Motion wird unterstützt!";
+    window.addEventListener('devicemotion', function (event) {
+        document.getElementById("xBeschl").innerHTML = 'x ' + event.acceleration.x.toFixed(2);
+        document.getElementById("yBeschl").innerHTML = 'y ' + event.acceleration.y.toFixed(2);
+        document.getElementById("zBeschl").innerHTML = 'z ' + event.acceleration.z.toFixed(2);
     });
-
 } else {
-    document.getElementById("dmeSupported").innerText = "Device Motion wird nicht unterstützt!";
+    document.getElementById("dmeSupported").innerText = "Device Motion wird nicht unterstützt!";
 }
