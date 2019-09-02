85e71f3a5c4faaf9f1551ceb6ee860c331fbaec1
frankmehler <36157035+frankmehler@users.noreply.github.com>
2019-09-02 06:37:23 +0200

    Add files via upload

--- js/meineDatei.js
+++ js/meineDatei.js
@@ -5,18 +5,35 @@
 /*eslint no-unused-vars: ["warn", { "varsIgnorePattern": "initMap|marker" }]*/
 
 function initMap() {
-    let thbingen = {
-        lat: 49.952381,
-        lng: 7.925409
-    };
-    let mapProp = {
-        center: new google.maps.LatLng(thbingen),
-        zoom: 15,
-    };
-    let meineKarte = new google.maps.Map(document.getElementById('googleMap'), mapProp);
-
-    let marker = new google.maps.Marker({
-        position: thbingen,
-        map: meineKarte
+    let directionsService = new google.maps.DirectionsService();
+    let directionsRenderer = new google.maps.DirectionsRenderer();
+    let map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 11,
+      center: {lat: 49.952381,
+        lng: 7.925409}
     });
-}
+    directionsRenderer.setMap(map);
+
+    let onChangeHandler = function() {
+      calculateAndDisplayRoute(directionsService, directionsRenderer);
+    };
+    document.getElementById('start').addEventListener('change', onChangeHandler);
+    document.getElementById('end').addEventListener('change', onChangeHandler);
+  }
+
+  function calculateAndDisplayRoute(directionsService, directionsRenderer) {
+      alert(document.getElementById('start').value);
+    directionsService.route(
+        {
+          origin: {placeId: document.getElementById('start').value},
+          destination: {placeId: document.getElementById('end').value},
+          travelMode: 'DRIVING'
+        },
+        function(response, status) {
+          if (status === 'OK') {
+            directionsRenderer.setDirections(response);
+          } else {
+            window.alert('Directions request failed due to ' + status);
+          }
+        });
+  }
