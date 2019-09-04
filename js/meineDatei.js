438662baef89318bdbfabe39fbdfe21813f28dbd
frankmehler <36157035+frankmehler@users.noreply.github.com>
2019-09-04 19:48:45 +0200

    Add files via upload

--- js/meineDatei.js
+++ js/meineDatei.js
@@ -16,20 +16,20 @@ function verschieben() {
 
 if (window.DeviceOrientationEvent) {
     document.getElementById("supported").innerText = "OK - Device Orient wird unterstützt!";
-    window.addEventListener('deviceorientation', ereignisEingetreten);
+    window.addEventListener('deviceorientation', setInterval(ereignisEingetreten, 3000));
 }
 else {
     document.getElementById("supported").innerText = "Keine Device Orient!";
 }
 
 function ereignisEingetreten(event) {
-    alert("Hallo");
+    alert("Hallo2");
     //document.getElementById("alpha").innerHTML = 'alpha ' + event.alpha.toFixed(2);
     document.getElementById("beta").innerHTML = 'beta ' + event.beta.toFixed(2);
     document.getElementById("gamma").innerHTML = 'gamma ' + event.gamma.toFixed(2);
     let x = event.beta;  // In degree in the range [-180,180] nach vorne - hinten +
     let y = event.gamma; // In degree in the range [-90,90] nach rechts + nach links -
-
+    //Transformation in positiven Bereich
     if (x > 0)
         x = Math.max(20, x);
     else
@@ -48,8 +48,8 @@ function ereignisEingetreten(event) {
     x = x / 10;
     y = y / 10;
 
-    ball.style.top = x + "px";
-    ball.style.left = y + "px";
+    ball.style.top = (ball.style.top + x) + "px";
+    ball.style.left = (ball.style.left + y) + "px";
 
     let ausgabe = "x: " + x + " beta: " + event.beta.toFixed(2) + "\n";
     ausgabe = ausgabe + "y: " + x + " gamma: " + event.gamma.toFixed(2) + "\n";
