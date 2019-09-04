69109ea90f90cd8db4e78f6e90b6a091168f88c3
frankmehler <36157035+frankmehler@users.noreply.github.com>
2019-09-04 19:13:57 +0200

    Add files via upload

--- js/meineDatei.js
+++ js/meineDatei.js
@@ -4,8 +4,8 @@ let ball = document.getElementById("meinBall");
 let garden = document.getElementById("meinSpiefeld");
 let output = document.getElementById("output");
 
-let maxX = garden.clientWidth - ball.clientWidth;
-let maxY = garden.clientHeight - ball.clientHeight;
+//let maxX = garden.clientWidth - ball.clientWidth;
+//let maxY = garden.clientHeight - ball.clientHeight;
 
 let meinButton = document.getElementById("myBtn");
 meinButton.addEventListener("click", verschieben);
@@ -16,36 +16,40 @@ function verschieben() {
 
 if (window.DeviceOrientationEvent) {
     document.getElementById("supported").innerText = "OK - Device Orient wird unterstützt!";
-    window.addEventListener('deviceorientation', function (event) {
-        //document.getElementById("alpha").innerHTML = 'alpha ' + event.alpha.toFixed(2);
-        document.getElementById("beta").innerHTML = 'beta ' + event.beta.toFixed(2);
-        document.getElementById("gamma").innerHTML = 'gamma ' + event.gamma.toFixed(2);
-        //let x = event.beta;  // In degree in the range [-180,180]
-        //let y = event.gamma; // In degree in the range [-90,90]
-        //alert ("maxX: " + maxX + " x: " + x);
+    window.addEventListener('deviceorientation', setInterval(ereignisEingetreten(event), 3000));
+}
 
+function ereignisEingetreten(event) {
+    //document.getElementById("alpha").innerHTML = 'alpha ' + event.alpha.toFixed(2);
+    document.getElementById("beta").innerHTML = 'beta ' + event.beta.toFixed(2);
+    document.getElementById("gamma").innerHTML = 'gamma ' + event.gamma.toFixed(2);
+    let x = event.beta;  // In degree in the range [-180,180] nach vorne - hinten +
+    let y = event.gamma; // In degree in the range [-90,90] nach rechts + nach links -
 
-        // 10 is half the size of the ball
-        // It center the positioning point to the center of the ball
-        // ball.style.top = (maxX * x / 180 - 10) + "px";
-        //ball.style.left = (maxY * y / 180 - 10) + "px";
-        let x = ball.style.top + event.beta / 10;
-        let y = ball.style.top + event.gamma / 10;
-        let ausgabe = "x: " + x + " beta: " + event.beta.toFixed(2) + "\n";
-        ausgabe = ausgabe + "y: " + x + " gamma: " + event.gamma.toFixed(2);
-        console.log("x: " + x + " beta: " + event.beta.toFixed(2));
-        console.log("y: " + x + " gamma: " + event.gamma.toFixed(2));
-        ball.style.top = Math.max(maxX, x) + "px";
-        ball.style.left = Math.max(maxY, y) + "px";
-        console.log("  " + ball.style.top);
-        console.log("  " + ball.style.left);
-        ausgabe = ausgabe + "  " + "  " + ball.style.top + "\n";        
-        ausgabe = ausgabe + "  " + ball.style.left + "\n";
-        output.innerHTML = ausgabe;
- 
-        //ball.style.top = Math.max(maxX * x / 180 - 10) + "px";
-        //ball.style.left = (maxY * y / 180 - 10) + "px";
-    });
-} else {
-    document.getElementById("supported").innerText = "Device Orient wird nicht unterstützt!";
+    if (x > 0)
+        x = Math.max(20, x);
+    else
+        x = Math.max(-20, x);
+
+    if (y > 0)
+        y = Math.max(20, y);
+    else
+        y = Math.max(-20, y);
+
+    if (ball.style.top > 100)
+        x = 100;
+    if (ball.style.left < -100)
+        y = -100;
+
+    x = x / 10;
+    y = y / 10;
+
+    ball.style.top = x + "px";
+    ball.style.left = y + "px";
+
+    let ausgabe = "x: " + x + " beta: " + event.beta.toFixed(2) + "\n";
+    ausgabe = ausgabe + "y: " + x + " gamma: " + event.gamma.toFixed(2) + "\n";
+    ausgabe = ausgabe + "  " + "  " + ball.style.top + "\n";
+    ausgabe = ausgabe + "  " + ball.style.left + "\n";
+    output.innerHTML = ausgabe;
 }
