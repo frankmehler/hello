94757c3827a89eab613bc596b08617d33c64c97a
frankmehler <36157035+frankmehler@users.noreply.github.com>
2019-09-04 08:28:57 +0200

    Add files via upload

--- js/meineDatei.js
+++ js/meineDatei.js
@@ -9,9 +9,9 @@ ctx.stroke();
 if (window.DeviceOrientationEvent) {
     document.getElementById("supported").innerText = "OK - Device Orient wird unterstützt!";
     window.addEventListener('deviceorientation', function(event) {
-        document.getElementById("xBeschl").innerHTML = 'alpha ' + event.alpha.toFixed(2);
-        document.getElementById("yBeschl").innerHTML = 'beta ' + event.beta;
-        document.getElementById("zBeschl").innerHTML = 'gamme ' + event.gamma;
+        document.getElementById("alpha").innerHTML = 'alpha ' + event.alpha.toFixed(2);
+        document.getElementById("beta").innerHTML = 'beta ' + event.beta;
+        document.getElementById("gamma").innerHTML = 'gamma ' + event.gamma;
     });
 } else {
     document.getElementById("supported").innerText = "Device Orient wird nicht unterstützt!";
