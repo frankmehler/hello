d3e371240034cacfe13ffdba60742736144c7c96
frankmehler <36157035+frankmehler@users.noreply.github.com>
2019-09-04 09:50:03 +0200

    Add files via upload

--- js/meineDatei.js
+++ js/meineDatei.js
@@ -4,7 +4,6 @@ let ball = document.getElementById("meinBall");
 let garden = document.getElementById("meinSpiefeld");
 
 let maxX = garden.clientWidth - ball.clientWidth;
-alert ("maxX: " + maxX);
 let maxY = garden.clientHeight - ball.clientHeight;
 
 let meinButton = document.getElementById("myBtn");
@@ -23,7 +22,7 @@ if (window.DeviceOrientationEvent) {
         let x = event.beta;  // In degree in the range [-180,180]
         let y = event.gamma; // In degree in the range [-90,90]
         y += 90;
-        alert ("maxX: " + maxX + " x: " + x);
+        //alert ("maxX: " + maxX + " x: " + x);
 
                // Because we don't want to have the device upside down
         // We constrain the x value to the range [-90,90]
@@ -34,7 +33,7 @@ if (window.DeviceOrientationEvent) {
         // x and y to [0,180]
         x += 90;
         y += 90;
-        alert ("maxX: " + maxX + " x: " + x);
+        //alert ("maxX: " + maxX + " x: " + x);
 
         // 10 is half the size of the ball
         // It center the positioning point to the center of the ball
