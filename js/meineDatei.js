781920003e0a74d350b8f211214a28a2de94a71c
frankmehler <36157035+frankmehler@users.noreply.github.com>
2019-09-04 11:56:15 +0200

    Add files via upload

--- js/meineDatei.js
+++ js/meineDatei.js
@@ -17,28 +17,28 @@ if (window.DeviceOrientationEvent) {
     document.getElementById("supported").innerText = "OK - Device Orient wird unterstützt!";
     window.addEventListener('deviceorientation', function (event) {
         document.getElementById("alpha").innerHTML = 'alpha ' + event.alpha.toFixed(2);
-        document.getElementById("beta").innerHTML = 'beta ' + event.beta;
-        document.getElementById("gamma").innerHTML = 'gamma ' + event.gamma;
-        let x = event.beta;  // In degree in the range [-180,180]
-        let y = event.gamma; // In degree in the range [-90,90]
-        y += 90;
+        document.getElementById("beta").innerHTML = 'beta ' + event.beta.toFixed(2);
+        document.getElementById("gamma").innerHTML = 'gamma ' + event.gamma.toFixed(2);
+        //let x = event.beta;  // In degree in the range [-180,180]
+        //let y = event.gamma; // In degree in the range [-90,90]
         //alert ("maxX: " + maxX + " x: " + x);
 
-               // Because we don't want to have the device upside down
-        // We constrain the x value to the range [-90,90]
-        if (x > 90) { x = 90; }
-        if (x < -90) { x = -90; }
-
-        // To make computation easier we shift the range of 
-        // x and y to [0,180]
-        x += 90;
-        y += 90;
-        //alert ("maxX: " + maxX + " x: " + x);
 
         // 10 is half the size of the ball
         // It center the positioning point to the center of the ball
-        ball.style.top = (maxX * x / 180 - 10) + "px";
-        ball.style.left = (maxY * y / 180 - 10) + "px";
+        // ball.style.top = (maxX * x / 180 - 10) + "px";
+        //ball.style.left = (maxY * y / 180 - 10) + "px";
+        let x = ball.style.top + event.beta / 10;
+        let y = ball.style.top + event.gamma / 10;
+        console.log("x: " + x + " beta: " + event.beta.toFixed(2));
+        console.log("y: " + x + " gamma: " + event.gamma.toFixed(2));
+        ball.style.top = Math.max(maxX, x) + "px";
+        ball.style.left = Math.max(maxY, y) + "px";
+        console.log("  " + ball.style.top);
+        console.log("  " + ball.style.top);
+ 
+        //ball.style.top = Math.max(maxX * x / 180 - 10) + "px";
+        //ball.style.left = (maxY * y / 180 - 10) + "px";
     });
 } else {
     document.getElementById("supported").innerText = "Device Orient wird nicht unterstützt!";
