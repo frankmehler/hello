ba1875e7662c61fccfad58fa56a9975bc1c9845d
frankmehler <36157035+frankmehler@users.noreply.github.com>
2019-09-05 13:44:29 +0200

    Add files via upload

--- js/meineDatei.js
+++ js/meineDatei.js
@@ -16,50 +16,44 @@ function verschieben() {
 
 if (window.DeviceOrientationEvent) {
     document.getElementById("supported").innerText = "OK - Device Orient wird unterstützt!";
-    window.addEventListener('deviceorientation', ereignisEingetreten);
+    window.addEventListener('deviceorientation', setInterval(ereignisEingetreten.bind(event), 3000));
 }
 else {
     document.getElementById("supported").innerText = "Keine Device Orient!";
 }
 
-function ereignisEingetreten() {
-    window.setInterval(handleEreignis, 3000);
-    //handleEreignis(event);
-}
+function ereignisEingetreten(event) {
+   document.getElementById("beta").innerHTML = 'beta ' + event.beta.toFixed(2);
+   document.getElementById("gamma").innerHTML = 'gamma ' + event.gamma.toFixed(2);
+   let x = event.beta;  // In degree in the range [-180,180] nach vorne - hinten +
+   let y = event.gamma; // In degree in the range [-90,90] nach rechts + nach links -
+   if (x > 0)
+       x = Math.max(20, x);
+   else
+       x = Math.max(-20, x);
 
-function handleEreignis(event) {
-    //document.getElementById("alpha").innerHTML = 'alpha ' + event.alpha.toFixed(2);
-    document.getElementById("beta").innerHTML = 'beta ' + event.beta.toFixed(2);
-    document.getElementById("gamma").innerHTML = 'gamma ' + event.gamma.toFixed(2);
-    let x = event.beta;  // In degree in the range [-180,180] nach vorne - hinten +
-    let y = event.gamma; // In degree in the range [-90,90] nach rechts + nach links -
-    if (x > 0)
-        x = Math.max(20, x);
-    else
-        x = Math.max(-20, x);
+   if (y > 0)
+       y = Math.max(20, y);
+   else
+       y = Math.max(-20, y);
+       
+   x = Math.round(x / 10);
+   y = Math.round(y / 10);
 
-    if (y > 0)
-        y = Math.max(20, y);
-    else
-        y = Math.max(-20, y);
+   if (ball.style.top > 200) ball.style.top = 190;
+   if (ball.style.top < 0) ball.style.top = 10;
+   if (ball.style.left > 200) ball.style.left = 190;
+   if (ball.style.left < 0) ball.style.left = 10;
 
-    if (ball.style.top > 200) ball.style.top = 190;
-    if (ball.style.top < 0) ball.style.top = 10;
-    if (ball.style.left > 200) ball.style.left = 190;
-    if (ball.style.left < 0) ball.style.left = 10;
+   ball.style.top = (ball.style.top + x) + "px";
+   ball.style.left = (ball.style.left + y) + "px";
 
-    x = Math.round(x / 10);
-    y = Math.round(y / 10);
-
-    ball.style.top = (ball.style.top + x) + "px";
-    ball.style.left = (ball.style.left + y) + "px";
-
-    let ausgabe = "x: " + x + " beta: " + event.beta.toFixed(2);
-    ausgabe = ausgabe + "y: " + y + " gamma: " + event.gamma.toFixed(2);
-    ausgabe = ausgabe + " top: " + ball.style.top;
-    ausgabe = ausgabe + " left: " + ball.style.left;
-    output.innerHTML = ausgabe;
-    alert("Hallo x " + ausgabe);
+   let ausgabe = "x: " + x + " beta: " + event.beta.toFixed(2);
+   ausgabe = ausgabe + "y: " + y + " gamma: " + event.gamma.toFixed(2);
+   ausgabe = ausgabe + " top: " + ball.style.top;
+   ausgabe = ausgabe + " left: " + ball.style.left;
+   output.innerHTML = ausgabe;
+   alert("Hallo aaa " + ausgabe);
 }
 
 
