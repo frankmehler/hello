ec46d08c6adb70eb2563170ba1b076f4ca212d0f
frankmehler <36157035+frankmehler@users.noreply.github.com>
2019-09-06 09:24:15 +0200

    Add files via upload

--- js/meineDatei.js
+++ js/meineDatei.js
@@ -15,11 +15,11 @@ function verschieben() {
 }
 
 if (window.DeviceOrientationEvent) {
-    document.getElementById("supported").innerText = "OK - Device Orient wird unterstützt!";
+    document.getElementById("supported").innerText = "OK - Device Orient wird unterstützt?!?!";
     window.addEventListener('deviceorientation', handleEvent);
 }
 else {
-    document.getElementById("supported").innerText = "Keine Device Orient!?!?";
+    document.getElementById("supported").innerText = "Keine Device Orient";
 }
 
 function handleEvent(event) {
@@ -29,8 +29,10 @@ function handleEvent(event) {
 function changeBall(beta, gamma) {
     let zeit = new Date();
     if (zeit.getSeconds() % 3 == 0) {
-        alert("Zeit " + zeit.getMinutes() + "." + zeit.getSeconds());
-        alert("beta: " + beta + " gamma: " + gamma);
+        document.getElementById("beta").innerHTML = 'beta ' + beta.toFixed(2);
+        document.getElementById("gamma").innerHTML = 'gamma ' + gamma.toFixed(2);
+        //alert("Zeit " + zeit.getMinutes() + "." + zeit.getSeconds());
+        //alert("beta: " + beta + " gamma: " + gamma);
     }
 
     /*
